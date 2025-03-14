8bfd9b19c98c6f17c4d234c4f7103d6b8ab0b741
WOLNOME <143981493+WOLNOME@users.noreply.github.com>
2025-03-14 14:17:18 +0900

    マージ完了

--- project/engine/scene/TitleScene.cpp
+++ project/engine/scene/TitleScene.cpp
@@ -28,7 +28,7 @@ void TitleScene::Finalize() {
 
 void TitleScene::Update() {
 	if (input_->TriggerKey(DIK_TAB)) {
-		sceneManager_->SetNextScene("CLEAR");
+		sceneManager_->SetNextScene("GAMEPLAY");
 	}
 
 	//カメラの更新
