b9cf86a007f2393236887caf943b536bdf66563f
WOLNOME <143981493+WOLNOME@users.noreply.github.com>
2025-03-14 14:10:46 +0900

    不足ファイルの追加

---
+++ project/engine/scene/GamePlayScene.cpp
@@ -0,0 +1,268 @@
+#include "GamePlayScene.h"
+
+#include "TextureManager.h"
+#include "ImGuiManager.h"
+#include "Object3dCommon.h"
+#include "ParticleCommon.h"
+#include "LineDrawerCommon.h"
+#include "SpriteCommon.h"
+#include "SceneManager.h"
+#include <numbers>
+
+void GamePlayScene::Initialize()
+{
+	//シーン共通の初期化
+	BaseScene::Initialize();
+
+	input_ = Input::GetInstance();
+
+	//カメラの生成と初期化
+	camera_ = std::make_unique<BaseCamera>();
+	camera_->Initialize();
+	camera_->SetRotate({ cameraRotate });
+	camera_->SetTranslate(cameraTranslate);
+	camera_->SetFarClip(100.0f);
+
+	// 当たり判定
+	appCollisionManager_ = AppCollisionManager::GetInstance();
+	appCollisionManager_->Initialize();
+
+	// スポーン位置
+	playerSpawnPositions_.push_back({ 0.0f,1.0f,0.0f });
+	playerSpawnPositions_.push_back({ 2.0f,1.0f,2.0f });
+	playerSpawnPositions_.push_back({ -2.0f,1.0f,-2.0f });
+
+	// プレイヤー
+	for (uint32_t i = 0; i < 1; ++i)
+	{
+		auto player = std::make_unique<Player>();
+
+		player->SetPlayerPos(playerSpawnPositions_[0]);
+		player->Initialize();
+
+		players_.push_back(std::move(player));
+	}
+
+	//エネミーマネージャーの生成と初期化
+	enemyManager_ = std::make_unique<EnemyManager>();
+	enemyManager_->Initialize(camera_.get(), &players_, "enemy");
+	//enemyManager_->SpawnTackleEnemy(7);
+	// フィールド
+	field_ = std::make_unique<Field>();
+	field_->Initialize();
+
+	// プレイヤースポーン位置モデル
+	for (uint32_t i = 0; i < playerSpawnNum_; ++i)
+	{
+		auto playerSpawn = std::make_unique<SpawnPos>();
+		playerSpawn->SetPosition(playerSpawnPositions_[i]);
+		playerSpawn->Initialize();
+	
+		playerSpawn_.push_back(std::move(playerSpawn));
+	}
+}
+
+void GamePlayScene::Finalize()
+{
+	for (auto& player : players_)
+	{
+		player->Finalize();
+	}
+	
+	enemyManager_->Finalize();
+
+	field_->Finalize();
+}
+
+void GamePlayScene::Update()
+{
+	// カメラの更新
+	camera_->UpdateMatrix();
+	camera_->SetRotate({ cameraRotate });
+	camera_->SetTranslate(cameraTranslate);
+
+	// 死んだプレイヤーを削除
+	players_.erase(std::remove_if(players_.begin(), players_.end(),
+		[](const std::unique_ptr<Player>& player)
+		{
+			if (player->IsDead())
+			{
+				player->Finalize();
+				return true;
+			}
+			return false;
+		}), players_.end());
+	// プレイヤー
+	for (auto& player : players_)
+	{
+		player->Update();
+	}
+
+
+	//エネミーマネージャーの更新
+	enemyManager_->Update();
+
+	// フィールド
+	field_->Update();
+
+	// プレイヤースポーンのオブジェクト
+	for (auto& playerSpawn : playerSpawn_)
+	{
+		playerSpawn->Update();
+	}
+	playerSpawnRotation();
+
+	// 当たり判定
+	appCollisionManager_->CheckAllCollision();
+
+	//タイトルシーンに戻る
+	if (input_->TriggerKey(DIK_ESCAPE)) {
+		sceneManager_->SetNextScene("TITLE");
+	}
+
+
+
+	// ImGui
+	ImGuiDraw();
+}
+
+void GamePlayScene::Draw()
+{
+	//3Dモデルの共通描画設定
+	Object3dCommon::GetInstance()->SettingCommonDrawing();
+
+	///------------------------------///
+	///↓↓↓↓モデル描画開始↓↓↓↓
+	///------------------------------///
+
+	// プレイヤー
+	for (auto& player : players_)
+	{
+        player->Draw(*camera_.get());
+	}
+
+	//エネミーマネージャーの描画
+	enemyManager_->Draw();
+
+	// フィールド
+	field_->Draw(*camera_.get());
+
+	// プレイヤースポーン
+	for (auto& playerSpawn : playerSpawn_)
+	{
+		playerSpawn->Draw(*camera_.get());
+	}
+
+
+	///------------------------------///
+	///↑↑↑↑モデル描画終了↑↑↑↑
+	///------------------------------///
+
+
+	//パーティクルの共通描画設定
+	ParticleCommon::GetInstance()->SettingCommonDrawing();
+
+	///------------------------------///
+	///↓↓↓↓パーティクル描画開始↓↓↓↓
+	///------------------------------///
+
+
+
+	///------------------------------///
+	///↑↑↑↑パーティクル描画終了↑↑↑↑
+	///------------------------------///
+
+
+	//線描画共通描画設定
+	LineDrawerCommon::GetInstance()->SettingCommonDrawing();
+
+	///------------------------------///
+	///↓↓↓↓線描画開始↓↓↓↓
+	///------------------------------///
+
+
+
+	///------------------------------///
+	///↑↑↑↑線描画終了↑↑↑↑
+	///------------------------------///
+
+	//スプライトの共通描画設定
+	SpriteCommon::GetInstance()->SettingCommonDrawing();
+
+	///------------------------------///
+	///↓↓↓↓スプライト描画開始↓↓↓↓
+	///------------------------------///
+
+
+
+	///------------------------------///
+	///↑↑↑↑スプライト描画終了↑↑↑↑
+	///------------------------------///
+}
+
+void GamePlayScene::ImGuiDraw()
+{
+
+#ifdef _DEBUG
+
+	ImGui::Begin("scene");
+	ImGui::Text("%s", "GAMEPLAY");;
+	
+	ImGui::SliderFloat3("cameraTranslate", &cameraTranslate.x, -100.0f, 100.0f);
+	ImGui::SliderFloat3("cameraRotate", &cameraRotate.x, -5.0f, 5.0f);
+
+	ImGui::SliderFloat3("playerSpawnPos", &playerSpawnPositions_[0].x, -10.0f, 10.0f);
+	// プレイヤーを追加するボタン
+	if (ImGui::Button("Add Player"))
+	{
+		auto player = std::make_unique<Player>();
+
+		player->SetPlayerPos(playerSpawnPositions_[0]);
+		player->Initialize();
+
+		players_.push_back(std::move(player));
+	}
+
+	ImGui::End();
+
+	// プレイヤー
+	for (auto& player : players_)
+	{
+		player->ImGuiDraw();
+	}
+
+	
+
+	// フィールド
+	field_->ImGuiDraw();
+
+
+
+#endif // _DEBUG
+
+}
+
+void GamePlayScene::playerSpawnRotation()
+{
+	// プレイヤースポーン位置のローテーション
+	rotationTimer_ -= 1.0f;
+	if (rotationTimer_ <= 0.0f)
+	{
+		rotationTimer_ = rotation_;
+
+		// プレイヤーを追加
+		auto player = std::make_unique<Player>();
+
+		player->SetPlayerPos(playerSpawnPositions_[playerSpawnIndex_]);
+		player->Initialize();
+
+		players_.push_back(std::move(player));
+
+		// 位置ローテを0に戻す
+		playerSpawnIndex_++;
+		if (playerSpawnIndex_ > playerSpawnNum_ - 1)
+		{
+			playerSpawnIndex_ = 0;
+		}
+	}
+}
---
+++ project/engine/scene/GamePlayScene.h
@@ -0,0 +1,85 @@
+#pragma once
+
+#include "BaseScene.h"
+#include "Sprite.h"
+#include "Object3d.h"
+#include "Audio.h"
+#include "Vector2.h"
+#include "Input.h"
+#include "LineDrawer.h"
+#include "DevelopCamera.h"
+
+#include <cstdint>
+#include <memory>
+
+// アプリケーションインクルード
+#include "../../application/objects/Player/Player.h"
+#include "../../application/objects/Player/SpawnPos/SpawnPos.h"
+#include "../../application/objects/Field/Field.h"
+#include "../../application/appCollider/AppCollisionManager.h"
+#include "../../application/enemy/EnemyManager.h"
+
+class GamePlayScene : public BaseScene
+{
+public:
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize() override;
+	/// <summary>
+	/// 終了時
+	/// </summary>
+	void Finalize() override;
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update() override;
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw() override;
+
+	// ImGui
+	void ImGuiDraw();
+
+	void playerSpawnRotation();
+
+private://メンバ変数
+
+	Input* input_ = nullptr;
+	
+	std::unique_ptr<BaseCamera> camera_ = nullptr;
+
+	Vector3 cameraTranslate = { 0.0f,70.0f,-50.0f };
+	Vector3 cameraRotate = { 0.95f,0.0f,0.0f };
+
+	// 当たり判定
+	AppCollisionManager* appCollisionManager_ = nullptr;
+
+	// プレイヤー
+	std::vector<std::unique_ptr<Player>> players_{};
+
+	//エネミーマネージャー
+	std::unique_ptr<EnemyManager> enemyManager_;
+
+	// フィールド
+	std::unique_ptr<Field> field_ = nullptr;
+
+
+	// プレイヤースポーン位置
+	std::vector<std::unique_ptr<SpawnPos>> playerSpawn_{};
+
+	// プレイヤースポーン位置の数(固定)
+	const uint32_t playerSpawnNum_ = 3;
+	// プレイヤースポーン(ローテーション用)
+	uint32_t playerSpawnIndex_ = 0;
+	// プレイヤースポーン位置
+	std::vector<Vector3> playerSpawnPositions_{};
+	// ローテーション間隔
+	const float rotation_ = 300.0f;
+	// ローテーション用タイマー
+	float rotationTimer_ = rotation_;
+
+
+};
+
