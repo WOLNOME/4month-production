f027e673e3b8c4a9e4ef7960dbc859e6c9fbf81d
kuriharakento <146906405+kuriharakento@users.noreply.github.com>
2025-03-26 12:59:56 +0900

    敵のモデル差し替え。

--- project/engine/scene/GamePlayScene.cpp
+++ project/engine/scene/GamePlayScene.cpp
@@ -46,7 +46,7 @@ void GamePlayScene::Initialize()
 
 	//エネミーマネージャーの生成と初期化
 	enemyManager_ = std::make_unique<EnemyManager>();
-	enemyManager_->Initialize(camera_.get(), &players_, "enemy");
+	enemyManager_->Initialize(camera_.get(), &players_, "enemy", "Fan","Freeze");
 	//enemyManager_->SpawnTackleEnemy(7);
 	
 	//スカイドーム
