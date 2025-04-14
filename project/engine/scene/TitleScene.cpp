aa712e9a2a0a304d56e97cc984242889fa259178
kuriharakento <146906405+kuriharakento@users.noreply.github.com>
2025-04-14 13:06:08 +0900

    プレイヤーの攻撃のクールダウンがリセットされない。

--- project/engine/scene/TitleScene.cpp
+++ project/engine/scene/TitleScene.cpp
@@ -1,5 +1,6 @@
 #include "TitleScene.h"
 #include "SceneManager.h"
+#include "application/appCollider/AppCollisionManager.h"
 
 void TitleScene::Initialize() {
 	//シーン共通の初期化
@@ -22,16 +23,28 @@ void TitleScene::Initialize() {
 
 	title_ = std::make_unique<TextWrite>();
 	title_->Initialize("TITLE");
-	title_->SetParam({ 320.0f,300.0f }, Font::OnionScript, 80.0f, { 1,1,0,1 });
+	title_->SetParam({ 320.0f,100.0f }, Font::OnionScript, 80.0f, { 1,1,0,1 });
 	title_->SetEdgeParam({ 1,0,0,1 }, 5.0f, 0.0f, true);
-	
+
+	//演出に使うプレイヤー
+	player_ = std::make_unique<Player>();
+	player_->Initialize();
+	player_->SetPlayerPos({ 0.0f,1.0f,0.0f });
+
+	//フィールド
+	field_ = std::make_unique<Field>();
+	field_->Initialize();
+
+	//当たり判定
+	appCollisionManager_ = AppCollisionManager::GetInstance();
+	appCollisionManager_->Initialize();
 }
 
 void TitleScene::Finalize() {
 }
 
 void TitleScene::Update() {
-	if (input_->TriggerKey(DIK_SPACE)) {
+	if (player_->IsDead()) {
 		sceneManager_->SetNextScene("STAGESELECT");
 	}
 
@@ -41,6 +54,44 @@ void TitleScene::Update() {
 #ifdef _DEBUG
 	ImGui::Begin("scene");
 	ImGui::Text("%s", "TITLE");
+	/*======カメラ========*/
+	//位置
+	Vector3 cameraTranslate = camera->GetTranslate();
+	ImGui::DragFloat3("cameraTranslate", &cameraTranslate.x, 0.1f);
+	camera->SetTranslate(cameraTranslate);
+	//回転
+	Vector3 cameraRotate = camera->GetRotate();
+	ImGui::DragFloat3("cameraRotate", &cameraRotate.x, 0.1f);
+	camera->SetRotate(cameraRotate);
+
+	/*======プレイヤー======*/
+	//プレイヤーの位置
+	Vector3 playerPos = player_->GetPosition();
+	ImGui::DragFloat3("playerTranslate", &playerPos.x, 0.1f);
+	player_->SetPosition(playerPos);
+	//プレイヤーのスケール
+	Vector3 playerScale = player_->GetScale();
+	ImGui::DragFloat3("playerScale", &playerScale.x, 0.1f);
+	player_->SetScale(playerScale);
+	//プレイヤーの回転
+	Vector3 playerRotate = player_->GetRotation();
+	ImGui::DragFloat3("playerRotate", &playerRotate.x, 0.1f);
+	player_->SetRotation(playerRotate);
+
+	/*======フィールド======*/
+	//フィールドの位置
+	Vector3 fieldPos = field_->GetPosition();
+	ImGui::DragFloat3("fieldTranslate", &fieldPos.x, 0.1f);
+	field_->SetPosition(fieldPos);
+	//フィールドのスケール
+	Vector3 fieldScale = field_->GetScale();
+	ImGui::DragFloat3("fieldScale", &fieldScale.x, 0.1f);
+	field_->SetScale(fieldScale);
+	//フィールドの回転
+	Vector3 fieldRotate = field_->GetRotation();
+	ImGui::DragFloat3("fieldRotate", &fieldRotate.x, 0.1f);
+	field_->SetRotation(fieldRotate);
+
 	ImGui::End();
 	//タイトルテキスト用のImGui
 	title_->DebugWithImGui();
@@ -49,6 +100,13 @@ void TitleScene::Update() {
 
 	spriteUI_->Update();
 
+	//プレイヤーの更新
+	player_->Update();
+	PlayerTackleCharge();
+	//フィールドの更新
+	field_->Update();
+	//当たり判定
+	appCollisionManager_->CheckAllCollision();
 }
 
 void TitleScene::Draw() {
@@ -59,7 +117,9 @@ void TitleScene::Draw() {
 	///↓↓↓↓モデル描画開始↓↓↓↓
 	///------------------------------///
 
+	player_->Draw(*camera.get());
 
+	field_->Draw(*camera.get());
 
 	///------------------------------///
 	///↑↑↑↑モデル描画終了↑↑↑↑
@@ -106,5 +166,22 @@ void TitleScene::TextDraw() {
 	///------------------------------///
 	///↑↑↑↑テキスト描画終了↑↑↑↑
 	///------------------------------///
+}
+
+void TitleScene::PlayerTackleCharge()
+{
+
+	// プレイヤーの攻撃フラグが立っているとき
+	if (player_->IsChargeMax())
+	{
+		charge_ += 1.0f / 60.0f;
+	}
+
+	// チャージが最大値に達したら
+	if (charge_ >= kChargeMax)
+	{
+		player_->SetIsChargeMax(false);
+		charge_ = 0.0f;
+	}
 }
 
--- project/engine/scene/TitleScene.h
+++ project/engine/scene/TitleScene.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "BaseScene.h"
 #include "DevelopCamera.h"
+#include "application/objects/Field/Field.h"
+#include "application/objects/Player/Player.h"
 
 class TitleScene : public BaseScene {
 public:
@@ -25,18 +27,28 @@ public:
 	/// </summary>
 	void TextDraw() override;
 
+private:
+	void PlayerTackleCharge();
+
 private://メンバ変数
 	Input* input_ = nullptr;
 
 	//タイトルスプライト
 	uint32_t textureHandleUI_ = 0u;
 	std::unique_ptr<Sprite> spriteUI_ = nullptr;
-
 	//開発用カメラ
 	std::unique_ptr<DevelopCamera> camera;
-	
 	//タイトル
 	std::unique_ptr<TextWrite> title_;
-
+	//演出に使うプレイヤー
+	std::unique_ptr<Player> player_ = nullptr;
+	//フィールド
+	std::unique_ptr<Field> field_ = nullptr;
+	//当たり判定
+	AppCollisionManager* appCollisionManager_ = nullptr;
+	//
+	float charge_ = 0.0f;
+	//
+	const float kChargeMax = 1.0f;
 };
 
