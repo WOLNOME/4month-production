e0e2925c377aa33e30be0e036e19fcf093135ed4
悠揮 牛尾 <k023g0109@g.neec.ac.jp>
2025-04-22 01:22:32 +0900

    ポーズ機能実装途中

--- project/application/PauseSystem.cpp
+++ project/application/PauseSystem.cpp
@@ -1,21 +1,47 @@
 #include "PauseSystem.h"
+#include "WinApp.h"
 #include "TextureManager.h"
+#include "RandomStringUtil.h"
 
 void PauseSystem::Initialize() {
 	//入力
 	input_ = Input::GetInstance();
+	//シーンマネージャー
+	sceneManager_ = SceneManager::GetInstance();
+
 	//スプライト
 	textureHandleCurtain = TextureManager::GetInstance()->LoadTexture("black.png");
 	spriteCurtain = std::make_unique<Sprite>();
 	spriteCurtain->Initialize(textureHandleCurtain);
 	spriteCurtain->SetColor({ 1.0f,1.0f,1.0f,0.55f });
 
+	textureHandleMenu = TextureManager::GetInstance()->LoadTexture("Flame.png");
+	for (int i = 0; i < (int)MenuState::kMaxNumMenuState; i++) {
+		spriteMenu_[i] = std::make_unique<Sprite>();
+		spriteMenu_[i]->Initialize(textureHandleMenu);
+		spriteMenu_[i]->SetAnchorPoint({ 0.5f,0.5f });
+		spriteMenu_[i]->SetSize({ 400.0f, 80.0f });
+		spriteMenu_[i]->SetColor({ 0.8f,0.8f,0.8f,1.0f });
+		spriteMenu_[i]->SetPosition({ WinApp::kClientWidth / 2.0f, 300.0f + (i * 130) });
+	}
+
 	//テキスト
 	textPause_ = std::make_unique<TextWrite>();
 	textPause_->Initialize("Pause");
-	textPause_->SetParam({ 320.0f,50.0f }, Font::OnionScript, 80.0f, { 1,1,1,1 });
-	textPause_->SetEdgeParam({ 0,0,0,1 }, 5.0f, 0.0f, true);
-	
+	textPause_->SetParam({ 415.0f,80.0f }, Font::Meiryo, 90.0f, { 1,1,1,1 });
+	textPause_->SetEdgeParam({ 0,0,0,1 }, 8.0f, { 0.004f,-0.004f }, true);
+
+	for (int i = 0; i < (int)MenuState::kMaxNumMenuState; i++) {
+		textMenu_[i] = std::make_unique<TextWrite>();
+		textMenu_[i]->Initialize("Menu" + RandomStringUtil::GenerateRandomString(3));
+		textMenu_[i]->SetParam({ WinApp::kClientWidth / 2.0f - 60.0f, 260.0f + (i * 130) }, Font::Meiryo, 60.0f, { 1,1,1,1 });
+		textMenu_[i]->SetEdgeParam({ 0,0,0,1 }, 8.0f, { 0.004f,-0.004f }, true);
+		//遊び方テキスト蚤の処理
+		if (i == (int)MenuState::HowToPlay) {
+			textMenu_[i]->SetPosition({ textMenu_[i]->GetPosition().x - 30.0f,textMenu_[i]->GetPosition().y });
+		}
+	}
+
 }
 
 void PauseSystem::Finalize() {
@@ -24,11 +50,38 @@ void PauseSystem::Finalize() {
 void PauseSystem::Update() {
 	//エスケープでポーズorポーズ解除
 	if (input_->TriggerKey(DIK_ESCAPE)) {
-		isPause_ = !isPause_;
+		if (isPause_) {
+			isPause_ = false; //ポーズ解除
+		}
+		else {
+			isPause_ = true; //ポーズ
+			//メニューの初期化
+			menuState_ = MenuState::Continue;
+			for (int i = 0; i < (int)MenuState::kMaxNumMenuState; i++) {
+				spriteMenu_[i]->SetSize({ 400.0f, 80.0f });
+				spriteMenu_[i]->SetColor({ 0.8f,0.8f,0.8f,1.0f });
+				textMenu_[i]->SetSize(60.0f);
+				textMenu_[i]->SetPosition({ WinApp::kClientWidth / 2.0f - 60.0f, 260.0f + (i * 130) });
+				//遊び方テキスト蚤の処理
+				if (i == (int)MenuState::HowToPlay) {
+					textMenu_[i]->SetPosition({ textMenu_[i]->GetPosition().x - 30.0f,textMenu_[i]->GetPosition().y });
+				}
+			}
+			spriteMenu_[0]->SetSize(spriteMenu_[0]->GetSize() * 1.2f);
+			spriteMenu_[0]->SetColor({ 1.0f,1.0f,0.8f,1.0f });
+			textMenu_[0]->SetSize(70.0f);
+			textMenu_[0]->SetPosition({ textMenu_[0]->GetPosition().x - 15.0f,textMenu_[0]->GetPosition().y - 5.0f });
+		}
 	}
 	//ポーズ中なら
 	if (isPause_) {
+		//メニュー操作
+		MenuAlgorithm();
+		//スプライトの更新
 		spriteCurtain->Update();
+		for (int i = 0; i < (int)MenuState::kMaxNumMenuState; i++) {
+			spriteMenu_[i]->Update();
+		}
 	}
 
 }
@@ -36,14 +89,24 @@ void PauseSystem::Update() {
 void PauseSystem::DrawSprite() {
 	//ポーズ中なら
 	if (isPause_) {
+		//薄暗いスプライトを描画
 		spriteCurtain->Draw();
+		//メニューのスプライトを描画
+		for (int i = 0; i < (int)MenuState::kMaxNumMenuState; i++) {
+			spriteMenu_[i]->Draw();
+		}
 	}
 }
 
 void PauseSystem::TextDraw() {
 	//ポーズ中なら
 	if (isPause_) {
+		//一時停止中
 		textPause_->WriteText(L"一時停止中");
+		//メニューのテキストを描画
+		textMenu_[0]->WriteText(L"続行");
+		textMenu_[1]->WriteText(L"遊び方");
+		textMenu_[2]->WriteText(L"終了");
 	}
 }
 
@@ -52,4 +115,92 @@ void PauseSystem::DebugWithImGui() {
 	//テキスト
 	textPause_->DebugWithImGui();
 #endif // _DEBUG
+}
+
+void PauseSystem::MenuAlgorithm() {
+	//メニュー操作
+	switch (menuState_) {
+	case PauseSystem::MenuState::Continue:
+		//操作
+		if (input_->TriggerKey(DIK_DOWN) || input_->TriggerKey(DIK_S)) {
+			//下に移動
+			menuState_ = PauseSystem::MenuState::HowToPlay;
+			//スプライトの編集
+			spriteMenu_[0]->SetColor({ 0.8f,0.8f,0.8f,1.0f });
+			spriteMenu_[0]->SetSize(spriteMenu_[0]->GetSize() / 1.2f);
+			spriteMenu_[1]->SetColor({ 1.0f,1.0f,0.8f,1.0f });
+			spriteMenu_[1]->SetSize(spriteMenu_[1]->GetSize() * 1.2f);
+			//テキストの編集
+			textMenu_[0]->SetSize(60.0f);
+			textMenu_[0]->SetPosition({ textMenu_[0]->GetPosition().x + 15.0f,textMenu_[0]->GetPosition().y + 5.0f });
+			textMenu_[1]->SetSize(70.0f);
+			textMenu_[1]->SetPosition({ textMenu_[1]->GetPosition().x - 15.0f,textMenu_[1]->GetPosition().y - 5.0f });
+		}
+		else if (input_->TriggerKey(DIK_SPACE)) {
+			//選択
+			isPause_ = false; //ポーズ解除
+		}
+
+		break;
+	case PauseSystem::MenuState::HowToPlay:
+		//操作
+		if (input_->TriggerKey(DIK_UP) || input_->TriggerKey(DIK_W)) {
+			//上に移動
+			menuState_ = PauseSystem::MenuState::Continue;
+			//スプライトの編集
+			spriteMenu_[1]->SetColor({ 0.8f,0.8f,0.8f,1.0f });
+			spriteMenu_[1]->SetSize(spriteMenu_[1]->GetSize() / 1.2f);
+			spriteMenu_[0]->SetColor({ 1.0f,1.0f,0.8f,1.0f });
+			spriteMenu_[0]->SetSize(spriteMenu_[0]->GetSize() * 1.2f);
+			//テキストの編集
+			textMenu_[0]->SetSize(70.0f);
+			textMenu_[0]->SetPosition({ textMenu_[0]->GetPosition().x - 15.0f,textMenu_[0]->GetPosition().y - 5.0f });
+			textMenu_[1]->SetSize(60.0f);
+			textMenu_[1]->SetPosition({ textMenu_[1]->GetPosition().x + 15.0f,textMenu_[1]->GetPosition().y + 5.0f });
+		}
+		else if (input_->TriggerKey(DIK_DOWN) || input_->TriggerKey(DIK_S)) {
+			//下に移動
+			menuState_ = PauseSystem::MenuState::Quit;
+			//スプライトの編集
+			spriteMenu_[1]->SetColor({ 0.8f,0.8f,0.8f,1.0f });
+			spriteMenu_[1]->SetSize(spriteMenu_[1]->GetSize() / 1.2f);
+			spriteMenu_[2]->SetColor({ 1.0f,1.0f,0.8f,1.0f });
+			spriteMenu_[2]->SetSize(spriteMenu_[2]->GetSize() * 1.2f);
+			//テキストの編集
+			textMenu_[1]->SetSize(60.0f);
+			textMenu_[1]->SetPosition({ textMenu_[1]->GetPosition().x + 15.0f,textMenu_[1]->GetPosition().y + 5.0f });
+			textMenu_[2]->SetSize(70.0f);
+			textMenu_[2]->SetPosition({ textMenu_[2]->GetPosition().x - 15.0f,textMenu_[2]->GetPosition().y - 5.0f });
+		}
+		else if (input_->TriggerKey(DIK_SPACE)) {
+
+		}
+		break;
+	case PauseSystem::MenuState::Quit:
+		//操作
+		if (input_->TriggerKey(DIK_UP) || input_->TriggerKey(DIK_W)) {
+			//上に移動
+			menuState_ = PauseSystem::MenuState::HowToPlay;
+			//スプライトの編集
+			spriteMenu_[2]->SetColor({ 0.8f,0.8f,0.8f,1.0f });
+			spriteMenu_[2]->SetSize(spriteMenu_[2]->GetSize() / 1.2f);
+			spriteMenu_[1]->SetColor({ 1.0f,1.0f,0.8f,1.0f });
+			spriteMenu_[1]->SetSize(spriteMenu_[1]->GetSize() * 1.2f);
+			//テキストの編集
+			textMenu_[1]->SetSize(70.0f);
+			textMenu_[1]->SetPosition({ textMenu_[1]->GetPosition().x - 15.0f,textMenu_[1]->GetPosition().y - 5.0f });
+			textMenu_[2]->SetSize(60.0f);
+			textMenu_[2]->SetPosition({ textMenu_[2]->GetPosition().x + 15.0f,textMenu_[2]->GetPosition().y + 5.0f });
+		}
+		else if (input_->TriggerKey(DIK_SPACE)) {
+			//セレクトシーンに戻る
+			sceneManager_->SetNextScene("STAGESELECT");
+		}
+		break;
+	case PauseSystem::MenuState::kMaxNumMenuState:
+		//何もしない
+		break;
+	default:
+		break;
+	}
 }
--- project/application/PauseSystem.h
+++ project/application/PauseSystem.h
@@ -1,9 +1,18 @@
 #pragma once
 #include "Input.h"
+#include "SceneManager.h"
 #include "Sprite.h"
 #include "TextWrite.h"
+#include <array>
 
 class PauseSystem {
+private:
+	enum class MenuState {
+		Continue,			//続行
+		HowToPlay,			//遊び方
+		Quit,				//終了
+		kMaxNumMenuState,	//メニューの数
+	};
 public:
 	/// <summary>
 	/// 初期化
@@ -34,17 +43,27 @@ public:
 	//ポーズ中かどうか
 	bool GetIsPause() const { return isPause_; }
 private:
+	//ポーズ中のメニュー操作
+	void MenuAlgorithm();
+private:
 	// 入力
 	Input* input_ = nullptr;
+	//シーンマネージャー
+	SceneManager* sceneManager_ = nullptr;
+
+
 	// スプライト
 	uint32_t textureHandleCurtain;
-	std::unique_ptr<Sprite> spriteCurtain = nullptr;
-	
+	std::unique_ptr<Sprite> spriteCurtain = nullptr;										//薄暗いスプライト
+	uint32_t textureHandleMenu;
+	std::array<std::unique_ptr<Sprite>, (int)MenuState::kMaxNumMenuState> spriteMenu_;		//メニューのスプライト
+
 	//テキスト
-	std::unique_ptr<TextWrite> textPause_ = nullptr;
+	std::unique_ptr<TextWrite> textPause_ = nullptr;										//一時停止中テキスト
+	std::array<std::unique_ptr<TextWrite>,(int)MenuState::kMaxNumMenuState> textMenu_;		//メニューのテキスト
 private:
 	bool isPause_ = false;		//ポーズ中かどうか
-
+	MenuState menuState_ = MenuState::Continue;	//メニューの状態
 
 };
 
